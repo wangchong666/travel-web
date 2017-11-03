a1cb5d14228723cfe27e23a4c2ff8c52ffe89e02
Administrator <420050757@qq.com>
2017-11-03 12:03:17 +0800

    add demo tpl

--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,11 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	
+	beego.SetStaticPath("/css", "static/css")
+	beego.SetStaticPath("/fonts", "static/fonts")
+	beego.SetStaticPath("/js", "static/js")
+	beego.SetStaticPath("/img", "static/img")
+	beego.SetStaticPath("/images", "static/images")
 }
